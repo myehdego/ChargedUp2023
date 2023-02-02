b3f8ead18530aa986ad7e251f42678429fa270a3
clydetheSlide <mizugana@gmail.com>
2023-02-02 12:37:25 -0500

    choose between comp and backup

--- CompBot/src/main/java/frc/robot/Constants.java
+++ CompBot/src/main/java/frc/robot/Constants.java
@@ -9,9 +9,13 @@ import edu.wpi.first.math.util.Units;
 
 public final class Constants {
   public static final boolean GRIPPER_AVAILABLE = false;
-  public static final boolean ARM_AVAILABLE = false;
-  public static final boolean PHOTONVISION_AVAILABLE = false;
-  public static final boolean PIXY_AVAILABLE = false;
+  public static final boolean ARM_AVAILABLE = true;
+  public static final boolean PHOTONVISION_AVAILABLE = true;
+  public static final boolean PIXY_AVAILABLE = true;
+  public static final boolean GRIPPER_AVAILABLE_Comp = false;
+  public static final boolean ARM_AVAILABLE_Comp = false;
+  public static final boolean PHOTONVISION_AVAILABLE_Comp = false;
+  public static final boolean PIXY_AVAILABLE_Comp = false;
   public static final int SYSTEMCHOOSER = 0;
 
 
